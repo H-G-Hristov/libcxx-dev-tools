c72794396d4a262037ce56e70c94e77fc00d91f3
Hristo Hristov <hghristov.rmm@gmail.com>
2025-03-13 16:07:30 +0200

    Update run_generator_scripts.sh

--- scripts/run_generator_scripts.sh
+++ scripts/run_generator_scripts.sh
@@ -24,6 +24,20 @@ main()
 
   # Run
   ninja -C "${build_dirpath}" libcxx-generate-files
+  ninja -C "${build_dirpath}" generate-cxx-abilist
+
+  echo "QUESTION: Do you want to run the transitive include generator script? (y/n)"
+  echo "          Set 'regenerate_expected_results = True' before running the generator script."
+  read -r answer
+
+  if [ "${answer}" != "${answer#[Yy]}" ]; then
+    # Set `regenerate_expected_results` to `True`, e.g. `regenerate_expected_results = True` in
+    #   libcxx/test/libcxx/transitive_includes.gen.py
+    for std in c++03 c++11 c++14 c++17 c++20 c++23 c++26;
+    do
+      "${build_dirpath}/bin/llvm-lit" --param std=$std libcxx/test/libcxx/transitive_includes.gen.py;
+    done
+  fi
 )
 
 ################################################################################
